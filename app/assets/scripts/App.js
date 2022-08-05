cdb83dea774305429221cac06eea59db018d58b5
Osamu <usamabinf@gmail.com>
2022-08-04 23:14:01 -0700

    Version 1.0 commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,14 @@
+import '../styles/styles.css'
+import 'lazysizes'
+
+// React Related Code Goes Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import React components
+
+// Other JS Code
+
+if (module.hot) {
+  module.hot.accept()
+}
